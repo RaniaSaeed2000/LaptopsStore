b2021606d4a2030880367aefb2445152b33a9eda
RaniaSaeed20 <ranosh.alsaeed.rs@gmail.com>
2021-07-01 11:26:54 +0300

    first app

---
+++ cart.js
@@ -0,0 +1,32 @@
+
+
+
+function plusOne() {
+    
+    var count = document.getElementById('count');
+  
+    count.value++
+  }
+
+  function minOne() {
+    
+    var count = document.getElementById('count');
+  
+    count.value--
+  }
+
+  function plusOne1() {
+    
+    var count1 = document.getElementById('topi');
+  
+    count1.value++
+  }
+
+
+
+  function minOne1() {
+    
+    var count1 = document.getElementById('topi');
+  
+    count1.value--
+  }
